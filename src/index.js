67396364c152654e13e1e67d12c7238d26542352
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2023-02-17 15:33:37 +0100

    babystep 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// React component
+function Card() {
+  return <h1>Hello React!</h1>;
+}
+
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
-  <h1>Hello React!</h1>
+  <Card></Card>
 )
