b84c563f46b82d4e30609362c9bf2e3385fd0fcb
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2023-02-17 15:32:17 +0100

    babystep 1

--- src/index.js
+++ src/index.js
@@ -1,53 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style.css';
 
-const animalsToAdopt = [
-  {
-      name: "Lucky",
-      picture: "https://placekitten.com/200/287",
-  },
-  {
-      name: "Symba",
-      picture: "https://placekitten.com/200/139",
-  },
-  {
-      name: "Léo",
-      picture: "https://placekitten.com/200/90",
-  },
-  {
-      name: "Milo",
-      picture: "https://placekitten.com/200/194",
-  },
-  {
-      name: "Charly",
-      picture: "https://placekitten.com/200/179",
-  },
-];
-
-const Card = (props) => {
-
-  return (
-    <div className="card">
-        <h4>{ props.name }</h4>
-        <img src="" alt=""></img>
-    </div>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-function animalsToReactComponents() {
-
-}
-
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
-  // [
-  //   <Card name={ animalsToAdopt[0].name } />,
-  //   <Card name={ animalsToAdopt[1].name } />,
-  //   <Card name={ animalsToAdopt[2].name } />,
-  //   <Card name={ animalsToAdopt[3].name } />,
-  //   <Card name={ animalsToAdopt[4].name } />,
-  // ]
-  animalsToAdopt.map( animalsToReactComponents )
-);
+  <h1>Hello React!</h1>
+)
