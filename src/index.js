dd4909301fb3f251228f94f9073ce034c33085eb
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2023-02-23 01:34:50 +0100

    commit assignment

--- src/index.js
+++ src/index.js
@@ -1,48 +1,60 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './style.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
 
 // the array of animals to adopt which are objects with two properties
 // each, a name and a picture
 const animalsToAdopt = [
   {
-      name: "Lucky",
-      picture: "https://placekitten.com/200/287",
+    name: "Lucky",
+    picture: "https://placekitten.com/200/287",
   },
   {
-      name: "Symba",
-      picture: "https://placekitten.com/200/139",
+    name: "Symba",
+    picture: "https://placekitten.com/200/139",
   },
   {
-      name: "Léo",
-      picture: "https://placekitten.com/200/90",
+    name: "Léo",
+    picture: "https://placekitten.com/200/90",
   },
   {
-      name: "Milo",
-      picture: "https://placekitten.com/200/194",
+    name: "Milo",
+    picture: "https://placekitten.com/200/194",
   },
   {
-      name: "Charly",
-      picture: "https://placekitten.com/200/179",
+    name: "Charly",
+    picture: "https://placekitten.com/200/179",
   },
 ];
 
-// The Card React Function component
-// expects two props: name and picture
-const Card = (props) => {
+// The Card React Function component, expects two props: "name" and "picture"
+const Card = ({name , picture}) => {
+  // "hidden" is a state variable that will continue to exist through the lifetime
+  // of the component. It has an initial value of "false" and can have its value
+  // changed at any time using the setter function "setHidden"
+  let [hidden, setHidden] = React.useState(false);
+  
+  function makeCardHidden() {
+    // TODO: implement me, use the "hidden" state to hide card, do not assign
+    // directly, use the setter function instead
+  }
+
+  // TODO: add conditional logic to hide card according to the value of the state
+  // variable "hidden"
   return (
     <div className="card">
-        <h4>{ props.name }</h4>
-        <img src={ props.picture } alt={ props.name }></img>
+      <h4>{name}</h4>
+      <img src={picture} alt={name}></img>
+      <br /><button onClick={makeCardHidden}>hide me</button>
     </div>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // this is the callback for the map expression in "render" below
-function animalsToReactComponents(object) {
-  return <Card name={object.name} picture={object.picture} />
+function animalsToReactComponents({name, picture}) {
+  return <Card name={name} picture={picture} />;
 }
 
 root.render(
@@ -54,8 +66,6 @@ root.render(
   //   a callback that takes the object and returns a JSX expression
   <>
     <h1>Adopt a Cat</h1>
-    <div className="deck">
-      { animalsToAdopt.map( animalsToReactComponents ) }
-    </div>
+    <div className="deck">{animalsToAdopt.map(animalsToReactComponents)}</div>
   </>
 );
